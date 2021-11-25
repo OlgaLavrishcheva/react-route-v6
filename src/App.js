243ff3da7d1529f574f60899d6ba58382fcaf1f1
OlgaLavrishcheva <76204278+OlgaLavrishcheva@users.noreply.github.com>
2021-11-25 16:24:06 +0200

    не видно содержания дочерних элементов

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+//не видно содержания дочерних элементов
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Users />} />
+        </Routes>
+      </BrowserRouter>
   );
+}
+
+function Users() {
+  return (
+      <div>
+          <h1>Home page</h1>
+        <nav>
+          <Link to="me">My Users</Link>
+          <Link to="UserProfile">My UserProfile</Link>
+          <Link to="OwnUserProfile">My OwnUserProfile</Link>
+        </nav>
+
+        <Routes>
+          <Route path=":id" element={<UserProfile />} />
+          <Route path="me" element={<OwnUserProfile />} />
+        </Routes>
+      </div>
+  );
+}
+
+function UserProfile() {
+    return (
+        <div>
+            <h2>UserProfile</h2>
+        </div>
+    );
+}
+
+function OwnUserProfile() {
+    return (
+        <div>
+            <h2>OwnUserProfile</h2>
+        </div>
+    );
 }
 
 export default App;
